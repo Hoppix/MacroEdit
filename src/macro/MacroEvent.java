0035cbc3faf8de26a99fe11e5d7e213b8aac6f29
Kolja Hopfmann <k.hopfmann@hotmail.de>
2016-12-21 19:53:59 +0100

    todo list

--- src/macro/MacroEvent.java
+++ src/macro/MacroEvent.java
@@ -8,14 +8,18 @@ import startupGUI.RecordAlertDialog;
 public class MacroEvent
 {
     String _name;
+
     //String in Key-Syntax
     String _macroString;
+
     //Commands aufgeteilt.
     String[] _actions;
     int[] _delay;
     String _syntax;
     String _keyword;
     String _miliseconds;
+
+    //Makro execute
     Robot robot;
 
     public MacroEvent(String name, String macroString)
@@ -23,6 +27,7 @@ public class MacroEvent
         _name = name;
         _macroString = macroString;
         _syntax = "({\\w+}-{\\d+(ms)}-)+";
+        //TODO regex in Java
 	    _keyword = "{\c*}";
         _miliseconds = "{\d*}";
         try
@@ -62,14 +67,17 @@ public class MacroEvent
     {
         if (recString.contains(_keyword))
         {
-            int start = recString.indexOf("{" + Actions.values());
+            //TODO exception handling
+            //TODO fill arrays
+            int start = recString.indexOf("{" + _keyword);
             int end = recString.indexOf("}", start);
             String sub = recString.substring(start + 1, end - 1);
-            _actions.recString.replaceFirst(_keyword, "");
+            recString.replaceFirst(_keyword, "");
 
             start = recString.indexOf("{" + _miliseconds);
             end = recString.indexOf("}", start);
             sub = recString.substring(start + 1, end - 1);
+            recString.replaceFirst(_miliseconds, "");
             readKeys(recString);
         }
     }
