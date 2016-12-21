572acae59a48a003d7f5944a886daf07a206fa84
Hoppix <k.hopfmann@hotmail.de>
2016-12-21 10:21:18 +0100

    progress in readkeys()

--- src/macro/MacroEvent.java
+++ src/macro/MacroEvent.java
@@ -11,8 +11,11 @@ public class MacroEvent
     //String in Key-Syntax
     String _macroString;
     //Commands aufgeteilt.
-    Actions[] _actions;
+    String[] _actions;
+    int[] _delay;
     String _syntax;
+    String _keyword;
+    String _miliseconds;
     Robot robot;
 
     public MacroEvent(String name, String macroString)
@@ -20,7 +23,8 @@ public class MacroEvent
         _name = name;
         _macroString = macroString;
         _syntax = "({\\w+}-{\\d+(ms)}-)+";
-
+	       _keyword = "{\c*}";
+         _miliseconds = "{\d*}";
         try
         {
             robot = new Robot();
@@ -36,7 +40,7 @@ public class MacroEvent
         parseRegex();
     }
 
-    public void parseRegex()
+    private void parseRegex()
     {
         //TODO parseEXCEPTION
         if (_macroString.matches(_syntax))
@@ -54,10 +58,21 @@ public class MacroEvent
         }
     }
 
-    private void readKeys()
+    private void readKeys(String recString)
     {
-        // TODO impl
+	if(recString.containts(_keyword)
+	{
+    int start = recString.indexOf("{" + Actions.values);
+		int end = recString.indexOf("}", start);
+		String sub  = recString.subString(start+1, end-1);
+    append(_actions, sub);
+    recString.replaceFirst(_keyword, "");
 
+    start = recString.indexOf("{" + _miliseconds);
+    end = recString.indexOf("}", start);
+    sub = recString.subString(start+1, end-1);
+		readKeys(recString);
+	}
     }
 
     public void execute()
